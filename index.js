15e3d9253a019b4dde0924474615d825ba8cb43c
smashweaver <jasonjimenez@live.com>
2016-05-30 08:06:27 +0800

    Update blueprint

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-nprogress'
+  name: 'ember-cli-nprogress',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/nprogress/nprogress.js');
+    app.import(app.bowerDirectory + '/nprogress/nprogress.css');
+  }
 };
