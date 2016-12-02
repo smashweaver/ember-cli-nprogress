c6ae792cd1939b33cacea1f1c178c3c9962a6147
Alex LaFroscia <alex@lafroscia.com>
2016-12-02 15:25:05 -0800

    Add unit test for the `nprogress` export

---
+++ tests/unit/utils/nprogress-test.js
@@ -0,0 +1,8 @@
+import nprogress from 'ember-cli-nprogress';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | nprogress');
+
+test('it imports the `nprogress` object', function(assert) {
+  assert.ok(nprogress);
+});
