40d7abdad95e38577d4a9bce0aced307f19a1ebb
Travis Hoover <thoov7@gmail.com>
2017-06-19 16:07:21 -0700

    Pull in deps from NPM over bower

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 /* jshint node: true */
 'use strict';
 
-var debug = require('debug')('ember-cli-nprogress:addon');
+const path = require('path');
+const map = require('broccoli-stew').map;
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
+const debug = require('debug')('ember-cli-nprogress:addon');
 
 /* identical to ember-cli/lib/models/addon.js's `_findHost` */
 /* used instead of breaking backwards compat. w/ older versions of cli */
@@ -26,14 +30,30 @@ module.exports = {
 
     var host = findHost(this);
 
-    host.import(host.bowerDirectory + '/nprogress/nprogress.js', {
+    host.import('vendor/nprogress/nprogress.js', {
       using: [
         { transformation: 'amd', as: 'nprogress' }
       ]
     });
 
-    host.import(host.bowerDirectory + '/nprogress/nprogress.css');
+    host.import('vendor/nprogress/nprogress.css');
 
     debug('Imported NProgress files!');
+  },
+
+  treeForVendor(vendorTree) {
+    const trees = [];
+    const nprogressTree = new Funnel(path.dirname(require.resolve('nprogress/nprogress.js')), {
+      files: ['nprogress.js', 'nprogress.css'],
+      destDir: 'nprogress'
+    });
+
+    if (vendorTree !== undefined) {
+      trees.push(vendorTree);
+    }
+
+    trees.push(nprogressTree);
+
+    return new MergeTrees(trees);
   }
 };
